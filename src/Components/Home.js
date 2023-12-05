f6374c6aa82cbe616ab29151944808399a05824e
Randy Rivera <rivera.na2015@gmail.com>
2023-12-05 17:53:13 -0500

    changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,9 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Content() {
+export default function Home() {
   return (
     <div>
-      <section class="">
+      <section className="">
         <div
           id="carouselExampleSlidesOnly"
           class="carousel slide"
@@ -26,32 +26,6 @@ function Content() {
                 style={{ height: "auto" }}
               />
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="bg-light" id="about">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-12 col-md-12">
-              <h3 class="text-center mt-4 text-secondary">
-                What's AnyMeal's Review site?
-              </h3>
-            </div>
-          </div>
-          <div class="row">
-            <p class="mt-4 mb-5">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum
-            </p>
           </div>
         </div>
       </section>
@@ -191,8 +165,18 @@ function Content() {
           </div>
         </div>
       </section>
+
+      <section class="bg-dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <h6 class="text-white text-center mb-3 mt-3">
+                Copyright @ AnyMeal's
+              </h6>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
-export default Content;
---
+++ src/layouts/HelpLayout.js
@@ -0,0 +1,20 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function HelpLayout() {
+  return (
+    <div className="help-layout">
+      <h2>Website Help</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+
+      <nav>
+        <NavLink to="faq">View the FAQ</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+}
